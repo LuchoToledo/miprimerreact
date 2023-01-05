85a2077149f2716a96a795a247883ee5cd023278
LuchoToledo <118233128+LuchoToledo@users.noreply.github.com>
2023-01-04 22:21:56 -0300

    Add files via upload

---
+++ mi-primer-react/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hellow Dojo! </h1>
+      <h3>Things I need to do :</h3>
+     
+      <ul>
+        <li>* learn react</li>
+        <li>* Climb Mt. everest</li>
+        <li>* Run a Marathon</li>
+        <li>* Feed the dog</li>
+      </ul>
+
+    </div>
+   
+  );
+}
+
+export default App;
